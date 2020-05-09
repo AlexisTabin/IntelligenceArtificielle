0e854d8e3798ef1faf4907436746b4d8318c2fb2
Alice <macbook@MacBook-Pro-de-Alice.local>
2020-05-09 10:59:05 +0200

    task 1 et task 2 dans 'projet_id3.py'

--- projet_id3.py
+++ projet_id3.py
@@ -2,7 +2,7 @@ from moteur_id3.noeud_de_decision import NoeudDeDecision
 from moteur_id3.id3 import ID3
 import csv
 
-#TASK 1
+#---------TASK 1----------
 
 def csv_reader(file):
 
@@ -33,20 +33,21 @@ def csv_reader(file):
                 target.append(row[-1]) #classe: valeur de 'target'
                 target.append({}) #attributs + valeurs
                 
-                for i in range(0,len(categories)):
+                for i in range(0,len(categories)-1):
                     target[1][categories[i]] = row[i]
                 donnees.append(target)
                 
             line_count += 1
         return donnees
 
+
 file_task1 = 'train_bin.csv'
 donnees_train = csv_reader(file_task1)
 
-
 id3 = ID3()
 arbre = id3.construit_arbre(donnees_train)
-print('Arbre de décision :')
+print('-----TASK 1-----')
+print('Arbre de décision:')
 print(arbre)
 print()
 
@@ -55,66 +56,50 @@ print()
 
 
 
-#TASK 2
+#---------TASK 2---------
 
 
-##def test_precision(file):
-##
-##    #recuperation des donnees du test
-##    donnees_test = csv_reader(file_task2)
-##
-##    #construction de l'arbre 
-##    id3 = ID3()
-##    arbre = id3.construit_arbre(donnees_train)
-##
-##    predictions_justes = 0
-##    predictions_fausses = 0
-##
-##    #verification de la preciction
-##    #for donnee in donnees_test:
-##
-##        #on enleve target de la donnee:
-####        sans_solution = donnee[1]
-####        solution = sans_solution["target"]
-####        del sans_solution["target"]
-####        
-####        full_prediction = arbre.classifie(donnee[1])
-####        print(full_prediction)
-##        
-##                
-##    print('Exemplification :')
-##    #full_prediction =
-##    print(arbre.classifie({
-##        'age': '3',
-##        'sex': '1',
-##        'cp': '1',
-##        'trestbps': '2',
-##        'chol': '1',
-##        'fbs': '1',
-##        'restecg': '0',
-##        'thalach': '3',
-##        'exang': '0',
-##        'oldpeak': '1',
-##        'slope': '2',
-##        'ca': '0',
-##        'thal': '2'}))
-##    
-##    
-##    
-##
-##    precision = predictions_justes #/(predictions_justes+predictions_fausses)*100
-##    
-##    return precision
-##
-##file_task2 = 'test_public_bin.csv'
-##
-##precision = test_precision(file_task2)
-##
-#id3 = ID3()
-#arbre = id3.construit_arbre(donnees_train)
-#print('Arbre de décision :')
-#print(arbre)
-#print()
+def test_precision(file):
+
+    #recuperation des donnees du test
+    donnees_test = csv_reader(file_task2)
+
+    #construction de l'arbre 
+    id3 = ID3()
+    arbre = id3.construit_arbre(donnees_train)
+
+    diagnostic_juste = 0
+    nb_diagnostics = len(donnees_test)
+    if nb_diagnostics == 0:
+        return 0
+    
+    #verification du diagnostic
+    for donnee in donnees_test:
+
+        symptomes = donnee[1]
+        solution = donnee[0]
+
+        diagnostic = arbre.classifie(symptomes)
+
+        #prediction fausse:
+        if diagnostic[-1] == solution:
+            diagnostic_juste += 1
+        
+
+              
+    precision = diagnostic_juste/(nb_diagnostics)*100
+    
+    return precision
+
+
+
+
+file_task2 = 'test_public_bin.csv'
+precision = test_precision(file_task2)
+
+print('-----TASK 2-----')
+print('Precision des diagnostics:')
+print(precision)
 
 
 
