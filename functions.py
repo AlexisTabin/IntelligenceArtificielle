3d3a28af71032b9a030bdea0acb1715ce6276b9d
Alice <macbook@MacBook-Pro-de-Alice.local>
2020-05-11 11:54:15 +0200

    task1 + task2 in project.py

--- functions.py
+++ functions.py
@@ -2,8 +2,6 @@ from moteur_id3.noeud_de_decision import NoeudDeDecision
 from moteur_id3.id3 import ID3
 import csv
 
-#---------TASK 1----------
-
 def csv_reader(file):
 
     """
@@ -41,39 +39,17 @@ def csv_reader(file):
         return donnees
 
 
-file_task1 = 'train_bin.csv'
-donnees_train = csv_reader(file_task1)
-
-id3 = ID3()
-arbre = id3.construit_arbre(donnees_train)
-print('-----TASK 1-----')
-print('Arbre de décision:')
-print(arbre)
-print()
-
-
-
-
-
-
-#---------TASK 2---------
-
-
-def test_precision(file):
+def test_precision(file, arbre):
 
     #recuperation des donnees du test
-    donnees_test = csv_reader(file_task2)
-
-    #construction de l'arbre 
-    id3 = ID3()
-    arbre = id3.construit_arbre(donnees_train)
+    donnees_test = csv_reader(file)
 
     diagnostic_juste = 0
     nb_diagnostics = len(donnees_test)
     if nb_diagnostics == 0:
         return 0
     
-    #verification du diagnostic
+    #verification du diagnostic p/r à l'arbre déjà établi
     for donnee in donnees_test:
 
         symptomes = donnee[1]
@@ -81,7 +57,7 @@ def test_precision(file):
 
         diagnostic = arbre.classifie(symptomes)
 
-        #prediction fausse:
+        #prediction correcte:
         if diagnostic[-1] == solution:
             diagnostic_juste += 1
         
@@ -90,16 +66,3 @@ def test_precision(file):
     precision = diagnostic_juste/(nb_diagnostics)*100
     
     return precision
-
-
-
-
-file_task2 = 'test_public_bin.csv'
-precision = test_precision(file_task2)
-
-print('-----TASK 2-----')
-print('Precision des diagnostics:')
-print(precision)
-
-
-
--- project.py
+++ project.py
@@ -1,3 +1,8 @@
+from functions import csv_reader
+from functions import test_precision
+from moteur_id3.noeud_de_decision import NoeudDeDecision
+from moteur_id3.id3 import ID3
+
 class ResultValues():
 
     def __init__(self):
@@ -5,7 +10,26 @@ class ResultValues():
         # Do computations here
         
         # Task 1
-        self.arbre = None
+        #self.arbre = None
+        file_task1 = 'train_bin.csv'
+        donnees_train = csv_reader(file_task1)
+
+        id3 = ID3()
+        arbre = id3.construit_arbre(donnees_train)
+        print('-----TASK 1-----')
+        print('Arbre de décision:')
+        print(arbre)
+        print()
+
+        # Task 2
+        file_task2 = 'test_public_bin.csv'
+        precision = test_precision(file_task2, arbre)
+
+        print('-----TASK 2-----')
+        print('Precision des diagnostics:')
+        print(precision)
+
+        
         # Task 3
         self.faits_initiaux = None
         self.regles = None
@@ -14,3 +38,5 @@ class ResultValues():
 
     def get_results(self):
         return [self.arbre, self.faits_initiaux, self.regles, self.arbre_advance]
+
+result = ResultValues()
