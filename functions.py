d984142fcdccbdb326a31ae4a876c4841b095ef3
AlexisTabin <alexis.tabin@gmail.com>
2020-05-13 16:00:22 +0200

    small refactorizations

--- functions.py
+++ functions.py
@@ -2,54 +2,51 @@ from moteur_id3.noeud_de_decision import NoeudDeDecision
 from moteur_id3.id3 import ID3
 import csv
 
-def csv_reader(file):
 
+def csv_reader(file):
     """
         :param string file: le fichier cvs que l'on veut lire
         :param list categories: la liste des noms de categorie
         :param list donnees: les données d'apprentissage\
         ``[classe, {attribut -> valeur}, ...]``
     """
-    
+
     categories = []
     donnees = []
-    
-    with open (file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ',')
+
+    with open(file) as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
-                    #rows_total = sum(1 for row in csv_reader)
-                    #print(f'there are {rows_total} lines')
-        
-        for row in csv_reader:
-                
-            if line_count == 0: #nom des catégories
+        # rows_total = sum(1 for row in csv_reader)
+        # print(f'there are {rows_total} lines')
+
+        for row in reader:
+
+            if line_count == 0:  # nom des catégories
                 categories = row
-                categories[0] = 'age'             
+                categories[0] = 'age'
             else:
-                #remplissage des colonnes de donnees
-                target = []
-                target.append(row[-1]) #classe: valeur de 'target'
-                target.append({}) #attributs + valeurs
-                
-                for i in range(0,len(categories)-1):
+                # remplissage des colonnes de donnees
+                target = [row[-1], {}]
+
+                for i in range(len(categories) - 1):
                     target[1][categories[i]] = row[i]
                 donnees.append(target)
-                
+
             line_count += 1
         return donnees
 
 
 def test_precision(file, arbre):
-
-    #recuperation des donnees du test
+    # recuperation des donnees du test
     donnees_test = csv_reader(file)
 
     diagnostic_juste = 0
     nb_diagnostics = len(donnees_test)
     if nb_diagnostics == 0:
         return 0
-    
-    #verification du diagnostic p/r à l'arbre déjà établi
+
+    # verification du diagnostic p/r à l'arbre déjà établi
     for donnee in donnees_test:
 
         symptomes = donnee[1]
@@ -57,12 +54,8 @@ def test_precision(file, arbre):
 
         diagnostic = arbre.classifie(symptomes)
 
-        #prediction correcte:
+        # prediction correcte:
         if diagnostic[-1] == solution:
             diagnostic_juste += 1
-        
 
-              
-    precision = diagnostic_juste/(nb_diagnostics)*100
-    
-    return precision
+    return diagnostic_juste / nb_diagnostics * 100
--- project.py
+++ project.py
@@ -1,16 +1,15 @@
 from functions import csv_reader
 from functions import test_precision
-from moteur_id3.noeud_de_decision import NoeudDeDecision
 from moteur_id3.id3 import ID3
 
-class ResultValues():
+
+class ResultValues:
 
     def __init__(self):
-        
         # Do computations here
-        
+
         # Task 1
-        #self.arbre = None
+        # self.arbre = None
         file_task1 = 'train_bin.csv'
         donnees_train = csv_reader(file_task1)
 
@@ -29,7 +28,6 @@ class ResultValues():
         print('Precision des diagnostics:')
         print(precision)
 
-        
         # Task 3
         self.faits_initiaux = None
         self.regles = None
@@ -38,5 +36,6 @@ class ResultValues():
 
     def get_results(self):
         return [self.arbre, self.faits_initiaux, self.regles, self.arbre_advance]
+
 
 result = ResultValues()
