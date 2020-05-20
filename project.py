93930c5f68838e97ae90be1e6a5522be7e29f1e4
Alice <macbook@MacBook-Pro-de-Alice.local>
2020-05-20 15:18:44 +0200

    debut task 5

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ from task_1.csc_reader import csv_reader
 from task_2.test_precision import test_precision
 from task_3.rule_generation import generateur_de_regles, derive_faits_depuis_fichier, justification
 from task_4.abduction import nb_patients_sauvables, diagnostic_et_prescription
+from task_5.id3_continous import ID3_continous
 
 
 class ResultValues:
@@ -13,8 +14,8 @@ class ResultValues:
         # Do computations here
 
         # Task 1
-        file_task1 = 'data/train_bin.csv'
-        donnees_train = csv_reader(file_task1)
+        self.file_task1 = 'data/train_bin.csv'
+        donnees_train = csv_reader(self.file_task1)
         self.id3 = ID3()
         self.arbre = self.id3.construit_arbre(donnees_train)
 
@@ -28,7 +29,21 @@ class ResultValues:
 
         # Task 4
 
+
         # Task 5
+        self.file_continous_train = 'data/train_continuous.csv'
+        donnees_continous = csv_reader(self.file_continous_train)
+        self.file_continous_test = 'data/test_public_continuous.csv'
+
+
+
+        self.id3_continous = ID3_continous()
+        self.arbre_continous = self.id3_continous.construit_arbre(donnees_continous)
+        self.precision_continous = test_precision(self.file_continous_test, self.arbre_continous)
+
+
+
+
         self.arbre_advance = None
 
         self.print_tasks()
@@ -72,11 +87,19 @@ class ResultValues:
         print("il y a {} personnes qui ne sont pas malades ".format(saufs))
         print("et {}heureusement {} personnes fichues".format("mal" if (fichus != 0) else '', fichus))
 
+    def print_task_5(self):
+        print('-----TASK 5------')
+        print(self.arbre_continous)
+        print()
+        print('Précision: '), print(self.precision_continous)
+
+
     def print_tasks(self):
-        self.print_task_1()
-        self.print_task_2()
-        self.print_task_3()
-        self.print_task_4()
+        #self.print_task_1()
+        #self.print_task_2()
+        #self.print_task_3()
+        #self.print_task_4()
+        self.print_task_5()
 
 
 result = ResultValues()
